e19bd73aa8ea41db516c25c57b3cfd9a521852b0
Lars Nysom <LANY@ucn.dk>
2022-09-21 13:27:12 +0200

    Complete code for demo session 04

--- ConnectionDemo/Program.cs
+++ ConnectionDemo/Program.cs
@@ -1,42 +1,44 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Transactions;
 
 Console.WriteLine("Connection Demo");
 
 SqlConnectionStringBuilder connStr = new();
 connStr.DataSource = "localhost\\sqlexpress";
 connStr.InitialCatalog = "ClientManager";
-connStr.Encrypt = false;
-
+connStr.Encrypt = false; // Must be false
 connStr.IntegratedSecurity = true;
 
 SqlConnection conn = new(connStr.ConnectionString);
-
-SqlCommand cmd = conn.CreateCommand();
-cmd.CommandText = "SELECT * FROM Customers";
-
 conn.Open();
 
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
+//SqlTransaction trans = conn.BeginTransaction();
+
+using TransactionScope scope = new TransactionScope();
+
+try
 {
+    SqlCommand cmd = conn.CreateCommand();
+    cmd.CommandText = "INSERT INTO Customers VALUES (@firstname, @lastname, @address, @zip, @city, @phone, @email)";
+    cmd.Parameters.AddWithValue("firstname", "Hans");
+    cmd.Parameters.AddWithValue("lastname", "Jensen");
+    cmd.Parameters.AddWithValue("address", "Bondegård");
+    cmd.Parameters.AddWithValue("zip", "9240");
+    cmd.Parameters.AddWithValue("city", "Nibe");
+    cmd.Parameters.AddWithValue("phone", "98351243");
+    cmd.Parameters.AddWithValue("email", "hj@gyllegym.dk");
+    //cmd.Transaction = trans;
+
+    int rowsAffedted = cmd.ExecuteNonQuery();
+
+    //trans.Commit();
+
+    //scope.Complete();
+}
+catch (Exception ex)
+{
+    // LOG ERROR!!!
+    //trans.Rollback();
    
-
-    Customer customer = new Customer();
-    customer.Name = reader["Firstname"] + " " + reader["Lastname"];
-
-    Console.WriteLine(customer);
-
 }
 
-
-conn.Close();
-
-class Customer
-{
-    public string Name { get; set; }
-
-    public override string ToString()
-    {
-        return Name;
-    }
-}
